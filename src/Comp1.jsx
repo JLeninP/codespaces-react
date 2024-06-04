38b2babb1794c9fce353d606a0e61f3374113eae
JLeninP <95876324+JLeninP@users.noreply.github.com>
2024-06-04 17:26:17 +0000

    se cambio los siguientes archivos

---
+++ src/Comp1.jsx
@@ -0,0 +1,7 @@
+import './estilos.css'
+function Uno(){
+    return(
+        <p className="c1">Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit ducimus voluptates molestias error earum, sunt dolorem eveniet? Consequatur assumenda eius optio ex eligendi quisquam, corrupti iure quas perferendis natus nesciunt!</p>
+
+    );
+}export default Uno;
