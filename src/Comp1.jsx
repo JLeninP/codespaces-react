05cd953cd3d70c76dc06ad2ff67959462a36dd7c
JLeninP <95876324+JLeninP@users.noreply.github.com>
2024-06-07 18:01:36 +0000

    Fix with wra-pcontent in css file (estilos.css)

--- src/Comp1.jsx
+++ src/Comp1.jsx
@@ -1,7 +1,16 @@
 import './estilos.css'
 function Uno(){
     return(
-        <p className="c1">Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit ducimus voluptates molestias error earum, sunt dolorem eveniet? Consequatur assumenda eius optio ex eligendi quisquam, corrupti iure quas perferendis natus nesciunt!</p>
-
+        <div className='firstBox'>
+            <div className='secondBox'>
+                <div className='text1'>
+                    <h1 className='c1'>Saludando Lenin Pocoaca</h1>
+                    <p className="c1">Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit ducimus voluptates molestias error earum, sunt dolorem eveniet? Consequatur assumenda eius optio ex eligendi quisquam, corrupti iure quas perferendis natus nesciunt!</p>
+                </div>
+                <div className="pic1">
+                    <img src="Octocat.png" className="App-logo" alt="logo" />
+                </div>
+            </div>
+        </div>
     );
 }export default Uno;
